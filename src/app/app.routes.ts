d743489cc0ff2a38eab3baf2c66dfb0832594f8a
Rahul Tiwari <rahul.tiwari@fdmgroup.com>
2024-03-07 18:06:12 -0500

    added notFound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AddAccountFormComponent } from './components/add-account-form/add-account-form.component';
 import { UpdateAccountComponent } from './components/update-account/update-account.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'addAccount', component: AddAccountFormComponent },
   { path: 'updateAccount/:accountId', component: UpdateAccountComponent },
+  { path: '**', component: NotFoundComponent },
 ];
--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
